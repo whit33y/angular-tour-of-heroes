136ff3fd9311d565a90736b958006d134add034a
whit33y <troppout@gmail.com>
2024-02-29 15:44:25 +0100

    List, conditional rendering, styles added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, UpperCasePipe, NgIf } from '@angular/common';
 import { Hero } from '../../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
