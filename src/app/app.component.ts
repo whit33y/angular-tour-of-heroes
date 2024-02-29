25b9b5350fc7ed53783adac1c3a1bd759ee78049
whit33y <troppout@gmail.com>
2024-02-29 12:41:29 +0100

    Created component, interface, pipe, formsmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Hero } from '../../hero';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [UpperCasePipe, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css',
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+}
