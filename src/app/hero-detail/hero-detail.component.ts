04e200508050c636a34f9e71b14366755ce86e54
whit33y <troppout@gmail.com>
2024-03-01 13:25:58 +0100

    Hero details by params and styles #2

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -20,4 +20,17 @@ export class HeroDetailComponent {
     private heroService: HeroService,
     private location: Location
   ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,11 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class HeroService {
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
   constructor(private messageService: MessageService) {}
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
