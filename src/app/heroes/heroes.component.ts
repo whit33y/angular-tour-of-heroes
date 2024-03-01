cc6ca7f5c5ded333b47e05f687861d2f9e3f5fd0
whit33y <troppout@gmail.com>
2024-03-01 10:38:19 +0100

    Added hero service with observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { Hero } from '../../hero';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,16 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
